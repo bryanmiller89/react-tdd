f5a4f150b700dbe644c8ac80febda5b62390c4fe
Bryan Miller <bryanmiller89@gmail.com>
2018-08-10 15:34:18 -0700

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <p>This is my Header</p>
+      </div>
+    );
+  }
+}
+
+export default Header;
